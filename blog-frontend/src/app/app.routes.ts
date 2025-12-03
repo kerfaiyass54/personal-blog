35f1a6fc68c78e23bb93f4b0f35f00cdeda9e300
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-12-03 20:43:44 +0100

    Added routes

--- blog-frontend/src/app/app.routes.ts
+++ blog-frontend/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import {LoginScreenComponent} from "./login-screen/login-screen.component";
+import {SignupScreenComponent} from "./signup-screen/signup-screen.component";
+import { AuthGuard } from './shared/guards/auth.guard';
+import { RoleGuard } from './shared/guards/role.guard';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'login', component: LoginScreenComponent },
+  { path: 'register', component: SignupScreenComponent },
+  { path: 'home', loadComponent: () => import('./home-page/home-page.component').then(c => c.HomePageComponent) },
+  {
+    path: 'writer',
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: ['Writer'] },
+    loadComponent: () => import('./writer-ui/writer-ui.component').then(c => c.WriterUiComponent)
+  },
+  { path: 'reader', canActivate: [AuthGuard], loadComponent: () => import('./reader-ui/reader-ui.component').then(c => c.ReaderUiComponent) },
+  { path: '**', redirectTo: 'home' }
+];
