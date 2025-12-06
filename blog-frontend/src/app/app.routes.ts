7a31a95c80d74ec3f01420182842865057b9b67e
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-12-06 21:13:54 +0100

    Added guard to the routes

--- blog-frontend/src/app/app.routes.ts
+++ blog-frontend/src/app/app.routes.ts
@@ -1,21 +1,24 @@
 import { Routes } from '@angular/router';
 import {LoginScreenComponent} from "./login-screen/login-screen.component";
 import {SignupScreenComponent} from "./signup-screen/signup-screen.component";
+import {HomePageComponent} from "./home-page/home-page.component";
 import { AuthGuard } from './shared/guards/auth.guard';
-import { RoleGuard } from './shared/guards/role.guard';
+import { WriterUiComponent} from './writer-ui/writer-ui.component';
+import { ReaderUiComponent} from './reader-ui/reader-ui.component';
+import {ErrorNotFoundComponent} from './error-not-found/error-not-found.component';
+
 
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', component: HomePageComponent },
   { path: 'login', component: LoginScreenComponent },
   { path: 'register', component: SignupScreenComponent },
-  { path: 'home', loadComponent: () => import('./home-page/home-page.component').then(c => c.HomePageComponent) },
   {
     path: 'writer',
-    canActivate: [AuthGuard, RoleGuard],
-    data: { roles: ['Writer'] },
-    loadComponent: () => import('./writer-ui/writer-ui.component').then(c => c.WriterUiComponent)
+    canActivate: [AuthGuard],
+    data: { roles: ['WRITER'] },
+    component: WriterUiComponent
   },
-  { path: 'reader', canActivate: [AuthGuard], loadComponent: () => import('./reader-ui/reader-ui.component').then(c => c.ReaderUiComponent) },
-  { path: '**', redirectTo: 'home' }
+  { path: 'reader', canActivate: [AuthGuard], component: ReaderUiComponent },
+  { path: '**', component: ErrorNotFoundComponent }
 ];
