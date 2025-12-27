b764253e60bdf215e992a0185e6dc1e8e2b449ee
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-12-27 13:56:09 +0100

    Added navbar for writers

--- blog-frontend/src/app/writer-ui/writer-ui.component.ts
+++ blog-frontend/src/app/writer-ui/writer-ui.component.ts
@@ -9,4 +9,12 @@ imports: [],
 })
 export class WriterUiComponent {
 
+  lessons:any[] = [{id: 0, title: 'Check',link: ''},
+    {id: 1, title: 'Add',link: ''}];
+  skills:any[] = [{id: 0, title: 'Check',link: ''},
+    {id: 1, title: 'Add',link: ''}];
+  articles:any[] = [{id: 0, title: 'Check',link: ''},
+    {id: 1, title: 'Summaries',link: ''},
+    {id: 2, title: 'Plans',link: ''}];
+
 }
