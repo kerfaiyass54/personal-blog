11161532555700555bdbf5d63d63057bf36e45dc
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-01-23 21:15:07 +0100

    Added components and bootstrap for the UI

---
+++ blog-frontend/src/app/shared/components/user-information/user-information.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-information',
+  imports: [],
+  templateUrl: './user-information.component.html',
+  styleUrl: './user-information.component.scss',
+})
+export class UserInformationComponent {
+
+}
---
+++ blog-frontend/src/app/shared/components/user-login-history/user-login-history.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-login-history',
+  imports: [],
+  templateUrl: './user-login-history.component.html',
+  styleUrl: './user-login-history.component.scss',
+})
+export class UserLoginHistoryComponent {
+
+}
