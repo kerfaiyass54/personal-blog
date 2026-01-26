001d57c19562d3bb5111d6d388f2d5ab9c0016b4
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-01-26 16:19:11 +0100

    Fixed navigation bar

---
+++ blog-frontend/src/app/login-history/login-history.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-history',
+  imports: [],
+  templateUrl: './login-history.component.html',
+  styleUrl: './login-history.component.scss',
+})
+export class LoginHistoryComponent {
+
+}
