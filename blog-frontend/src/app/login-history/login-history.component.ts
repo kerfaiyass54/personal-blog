94889ce8aebb2d430526fb7159586d1fb6833e89
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-02-10 22:28:27 +0100

    Linked table to the details

--- blog-frontend/src/app/login-history/login-history.component.ts
+++ blog-frontend/src/app/login-history/login-history.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {SessionsManagementService} from "../shared/services/sessions-management.service";
 import {TableComponent} from "../components/table/table.component";
+import {Route, Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-history',
@@ -22,23 +23,26 @@ export class LoginHistoryComponent implements OnInit{
   selectedSession: any[] | null = null;
   showModal = false;
   id: any = '';
+  role: any = '';
 
   handleRowClick(row: any[]) {
     this.selectedSession = row;
     this.showModal = true;
+    let url = '/' + this.role + '/session-details/' + row[0];
+    this.route.navigate([url]);
   }
 
 
 
-  constructor(private sessionsService: SessionsManagementService) {
+  constructor(private sessionsService: SessionsManagementService, private route: Router) {
   }
 
   ngOnInit() {
+    this.role = sessionStorage.getItem("role")?.toLowerCase();
     this.email = sessionStorage.getItem("email");
     this.sessionsService.getAllSessions(this.email).subscribe(
       (sessions)=>{
         this.sessions = sessions;
-        console.log(this.sessions);
         this.tableData = sessions.map(s => [
           s.id,
           s.os,
