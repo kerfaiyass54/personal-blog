ecffc5162907aa90c3bdefc6d5ba26edf23d8a5d
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-02-11 15:12:23 +0100

    Added button for navigation

---
+++ blog-frontend/src/app/components/button/button.component.ts
@@ -0,0 +1,15 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-button',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss',
+})
+export class ButtonComponent {
+  @Input() text: any = '';
+  @Input() link: any = '';
+}
