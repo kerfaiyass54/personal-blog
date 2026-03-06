02679531395d18797576341241d9af2efd6b0795
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-03-06 21:48:48 +0100

    Added exception handling

---
+++ blog-backend/src/main/java/com/blogproject/blogproject/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.blogproject.blogproject.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
