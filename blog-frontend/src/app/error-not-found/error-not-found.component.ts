127f2e5ed39167b4cf83ad8a790bdf256312292c
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-03-08 15:08:18 +0100

    Added error page

--- blog-frontend/src/app/error-not-found/error-not-found.component.ts
+++ blog-frontend/src/app/error-not-found/error-not-found.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import {ButtonComponent} from "../components/button/button.component";
+import { Location } from '@angular/common';
+
 
 @Component({
     selector: 'app-error-not-found',
     standalone: true,
-imports: [],
+  imports: [
+    ButtonComponent
+  ],
     templateUrl: './error-not-found.component.html',
     styleUrl: './error-not-found.component.scss'
 })
 export class ErrorNotFoundComponent {
+
+  constructor(private location: Location) {
+  }
+
+  goBack() {
+    this.location.back();
+  }
 
 }
