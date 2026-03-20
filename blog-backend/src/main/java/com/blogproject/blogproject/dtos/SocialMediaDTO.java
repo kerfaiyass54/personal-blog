fa865b9163f61d3569b147cc81256b5ac7ff03be
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-03-20 16:20:34 +0100

    Added a new entity for social media with its dto

---
+++ blog-backend/src/main/java/com/blogproject/blogproject/dtos/SocialMediaDTO.java
@@ -0,0 +1,18 @@
+package com.blogproject.blogproject.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class SocialMediaDTO {
+
+    private String id;
+    private String name;
+    private String link;
+}
--- blog-backend/src/main/java/com/blogproject/blogproject/entities/SocialMedia.java
+++ blog-backend/src/main/java/com/blogproject/blogproject/entities/SocialMedia.java
@@ -1,8 +1,9 @@
 package com.blogproject.blogproject.entities;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
@@ -12,7 +13,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "social_medias")
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
 @Getter
 @Setter
 public class SocialMedia {
@@ -20,7 +20,12 @@ public class SocialMedia {
 
     @Id
     private String id;
+
+    @NotBlank(message = "Name cannot be empty")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
     private String name;
+
+    @NotBlank(message = "Link cannot be empty")
     private String link;
 
 
