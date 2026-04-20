7bee55b45ee10fcaaa415dbad220848d18652c4c
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-04-20 22:30:24 +0100

    Added Ui dashboard with services

--- blog-frontend/src/app/user-playlists/user-playlists.component.ts
+++ blog-frontend/src/app/user-playlists/user-playlists.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import {PlaylistServicesService} from "./service/playlist-services.service";
+import {SoundtrackServicesService} from "./service/soundtrack-services.service";
+
 
 @Component({
   selector: 'app-user-playlists',
@@ -13,8 +16,74 @@ export class UserPlaylistsComponent implements OnInit {
 
   role: string | undefined = '';
 
+  email: string | null = null;
+
+  playlistsCount: number = 0;
+
+  playlistTracksCount: number = 0;
+
+  soundtracksCount: number = 0;
+
+  ratedSoundtracksCount: number = 0;
+
+
+  constructor(
+    private playlistService: PlaylistServicesService,
+    private soundtrackService: SoundtrackServicesService
+  ) {}
+
+
   ngOnInit() {
-    this.role = sessionStorage.getItem('role')?.toLowerCase();
+
+    this.role = sessionStorage
+      .getItem('role')
+      ?.toLowerCase() || '';
+
+    this.email = sessionStorage
+      .getItem('email');
+
+    if (!this.email) return;
+
+
+    // ✅ playlists count
+    this.playlistService
+      .getPlaylistsCount(this.email)
+      .subscribe(res => {
+
+        this.playlistsCount = res;
+
+      });
+
+
+    // ✅ tracks inside playlists count
+    this.playlistService
+      .getInsertedTracksCount(this.email)
+      .subscribe(res => {
+
+        this.playlistTracksCount = res;
+
+      });
+
+
+    // ✅ soundtracks count
+    this.soundtrackService
+      .getTotalSoundtracks(this.email)
+      .subscribe(res => {
+
+        this.soundtracksCount = res;
+
+      });
+
+
+    // ✅ rated soundtracks count
+    this.soundtrackService
+      .getRatedSoundtracks(this.email)
+      .subscribe(res => {
+
+        this.ratedSoundtracksCount = res;
+
+      });
+
   }
 
 }
