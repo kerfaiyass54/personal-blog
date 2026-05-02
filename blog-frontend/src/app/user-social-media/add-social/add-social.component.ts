fe395a8e3c954de8cdb930a8c23dc668a64b33bb
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-05-02 18:19:05 +0100

    Added fixes

--- blog-frontend/src/app/user-social-media/add-social/add-social.component.ts
+++ blog-frontend/src/app/user-social-media/add-social/add-social.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-add-social',
   imports: [],
+  standalone: true,
   templateUrl: './add-social.component.html',
   styleUrl: './add-social.component.scss',
 })
