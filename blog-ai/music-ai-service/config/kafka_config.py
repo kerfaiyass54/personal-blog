47a8481a0a902346e6a017a7acb7b190fbe9a947
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-05-09 13:28:29 +0100

    Added kafka config

---
+++ blog-ai/music-ai-service/config/kafka_config.py
@@ -0,0 +1,16 @@
+from kafka import KafkaConsumer, KafkaProducer
+from config.settings import KAFKA_BROKER
+import json
+
+producer = KafkaProducer(
+    bootstrap_servers=KAFKA_BROKER,
+    value_serializer=lambda v: json.dumps(v).encode("utf-8")
+)
+
+consumer = KafkaConsumer(
+    "playlist-events",
+    bootstrap_servers=KAFKA_BROKER,
+    auto_offset_reset="latest",
+    group_id="music-ai-group",
+    value_deserializer=lambda x: json.loads(x.decode("utf-8"))
+)
