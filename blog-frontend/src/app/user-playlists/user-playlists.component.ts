11fa4e48113384e7a1a0c3ea5d391b568ddd5b2c
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-05-13 16:59:28 +0100

    Updated soundtracks page by addding recommendations card

--- blog-frontend/src/app/user-playlists/user-playlists.component.ts
+++ blog-frontend/src/app/user-playlists/user-playlists.component.ts
@@ -1,21 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from "@angular/router";
-import {PlaylistServicesService} from "./service/playlist-services.service";
-import {SoundtrackServicesService} from "./service/soundtrack-services.service";
-
+import { PlaylistServicesService } from "./service/playlist-services.service";
+import { SoundtrackServicesService } from "./service/soundtrack-services.service";
 
 @Component({
   selector: 'app-user-playlists',
+  standalone: true,
   imports: [
     RouterLink
   ],
-  standalone: true,
   templateUrl: './user-playlists.component.html',
   styleUrl: './user-playlists.component.scss',
 })
 export class UserPlaylistsComponent implements OnInit {
 
-  role: string | undefined = '';
+  role: string = '';
 
   email: string | null = null;
 
@@ -27,14 +26,12 @@ export class UserPlaylistsComponent implements OnInit {
 
   ratedSoundtracksCount: number = 0;
 
-
   constructor(
     private playlistService: PlaylistServicesService,
     private soundtrackService: SoundtrackServicesService
   ) {}
 
-
-  ngOnInit() {
+  ngOnInit(): void {
 
     this.role = sessionStorage
       .getItem('role')
@@ -46,7 +43,7 @@ export class UserPlaylistsComponent implements OnInit {
     if (!this.email) return;
 
 
-    // ✅ playlists count
+    // PLAYLISTS COUNT
     this.playlistService
       .getPlaylistsCount(this.email)
       .subscribe(res => {
@@ -56,7 +53,7 @@ export class UserPlaylistsComponent implements OnInit {
       });
 
 
-    // ✅ tracks inside playlists count
+    // TRACKS COUNT
     this.playlistService
       .getInsertedTracksCount(this.email)
       .subscribe(res => {
@@ -66,7 +63,7 @@ export class UserPlaylistsComponent implements OnInit {
       });
 
 
-    // ✅ soundtracks count
+    // SOUNDTRACKS COUNT
     this.soundtrackService
       .getTotalSoundtracks(this.email)
       .subscribe(res => {
@@ -76,7 +73,7 @@ export class UserPlaylistsComponent implements OnInit {
       });
 
 
-    // ✅ rated soundtracks count
+    // RATED SOUNDTRACKS COUNT
     this.soundtrackService
       .getRatedSoundtracks(this.email)
       .subscribe(res => {
