cd55f2d6f43c5a2ec4eb40d0a36507e975eced8e
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-09-18 22:28:39 +0100

    Made the application zoneless

--- blog-frontend/src/app/app.config.ts
+++ blog-frontend/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { routes } from './app.routes';
@@ -6,5 +6,12 @@ import {JwtInterceptor} from "./shared/interceptors/jwt.interceptor";
 import {provideToastr} from "ngx-toastr";
 import {provideAnimations} from "@angular/platform-browser/animations";
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },provideHttpClient(),provideToastr(),provideAnimations()]
+  providers: [
+    provideZonelessChangeDetection(),
+    provideRouter(routes),
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    provideHttpClient(),
+    provideToastr(),
+    provideAnimations()
+  ]
 };
