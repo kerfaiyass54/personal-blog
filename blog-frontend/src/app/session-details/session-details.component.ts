7792a0d53272edb66bdfce08d795960afadc8c40
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-09-19 20:27:47 +0100

    Added sessions details

--- blog-frontend/src/app/session-details/session-details.component.ts
+++ blog-frontend/src/app/session-details/session-details.component.ts
@@ -1,7 +1,7 @@
-import { ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
-import {SessionsManagementService} from "../shared/services/sessions-management.service";
+import {SessionDetails, SessionsManagementService} from "../shared/services/sessions-management.service";
 import {InformationCardComponent} from "../components/information-card/information-card.component";
 import {ButtonComponent} from "../components/button/button.component";
 
@@ -25,30 +25,49 @@ export class SessionDetailsComponent implements OnInit{
   time: any = '';
   isMe: boolean = false;
   alert: string = '';
+  loading = true;
+  loadError = '';
 
-  constructor(private activeRouter: ActivatedRoute, private sessionsService: SessionsManagementService, private router: Router) {
+  constructor(
+    private activeRouter: ActivatedRoute,
+    private sessionsService: SessionsManagementService,
+    private router: Router,
+    private cdr: ChangeDetectorRef
+  ) {
   }
 
   ngOnInit() {
     this.id = this.activeRouter.snapshot.paramMap.get('id');
     localStorage.setItem("sessionID",this.id);
     this.role = sessionStorage.getItem("role")?.toLowerCase();
-    this.sessionsService.getSession(this.id).subscribe(
-      (session)=>{
-        console.log(session);
+    if (!this.id) {
+      this.loadError = 'The requested session was not found.';
+      this.loading = false;
+      return;
+    }
+
+    this.sessionsService.getSession(this.id).subscribe({
+      next: (session: SessionDetails) => {
         this.browser.value = session.browser;
         this.os.value = session.os;
-        this.time = session.time;
+        this.time = new Date(session.time).toLocaleString();
         this.alert = session.alert;
         this.isMe = session.me;
+        this.loading = false;
+        this.cdr.markForCheck();
+      },
+      error: () => {
+        this.loading = false;
+        this.loadError = 'Unable to load this login session.';
+        this.cdr.markForCheck();
       }
-    )
+    });
   }
 
   notMe(){
     this.sessionsService.setIsItMe(this.id,false).subscribe(
       ()=>{
-        this.router.navigate(['/reader/password']);
+        this.router.navigate(['/' + this.role + '/password']);
       }
     );
   }
